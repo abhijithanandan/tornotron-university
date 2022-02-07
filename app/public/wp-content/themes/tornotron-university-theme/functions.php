d09e5ff78c1654d770b1897d78645a84afbc75bb
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2022-02-07 21:29:19 +0530

    Added the style sheet files and included them in function.php

--- app/public/wp-content/themes/tornotron-university-theme/functions.php
+++ app/public/wp-content/themes/tornotron-university-theme/functions.php
@@ -2,6 +2,7 @@
 
 function university_files() {
     wp_enqueue_style('university_main_style', get_stylesheet_uri()); //hooks
+    wp_enqueue_style('university_additional_style', get_theme_file_uri('index.css')); //hooks
 }
 
 ?> 
