bf5d922491dc76c2779eab17b0682513fcfae3d0
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2022-05-22 21:46:31 +0530

    removed hard coded value of posts page title

--- app/public/wp-content/themes/tornotron-university-theme/index.php
+++ app/public/wp-content/themes/tornotron-university-theme/index.php
@@ -7,7 +7,7 @@ get_header();
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"> Blog </h1>
+    <h1 class="page-banner__title"> <?php single_post_title(); ?> </h1>
     <div class="page-banner__intro">
       <p>Latest posts from Tornotron Univeristy</p>
     </div>
