8fbbc998ff9c721332408556e1c0298746f2b321
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2022-06-01 21:56:16 +0530

    Created event archives

--- app/public/wp-content/themes/tornotron-university-theme/single-event.php
+++ app/public/wp-content/themes/tornotron-university-theme/single-event.php
@@ -20,7 +20,7 @@ while (have_posts()) {
     <div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-            <a class="metabox__blog-home-link" href="<?php echo site_url('/event'); ?>"> <i class="fa fa-home" aria-hidden="true"></i> Event Home </a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('d-m-Y'); ?> in <?php echo get_the_category_list(', '); ?></span>
+            <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"> <i class="fa fa-home" aria-hidden="true"></i> Event Home </a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('d-m-Y'); ?> in <?php echo get_the_category_list(', '); ?></span>
         </p>
     </div>
         <div class="geneic-content">
