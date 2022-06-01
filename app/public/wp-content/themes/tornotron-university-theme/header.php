7f55c576bdfdd2d7a48ff02cce2ace1e1955174e
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2022-06-01 22:37:32 +0530

    Added excrept support to event and enabled rest api

--- app/public/wp-content/themes/tornotron-university-theme/header.php
+++ app/public/wp-content/themes/tornotron-university-theme/header.php
@@ -27,9 +27,9 @@
                   //     'theme_location' => 'headerMenuLocation'
                   // ));
                 ?> 
-                <li><a href="#">About Us</a></li>
+                <li <?php if(get_the_ID() == '14' OR wp_get_post_parent_id() == '14') {echo 'class="current-menu-item"';} ?>><a href="<?php echo site_url('/about-us')?>">About Us</a></li>
                 <li><a href="#">Programs</a></li>
-                <li><a href="#">Events</a></li>
+                <li <?php if(get_post_type() == 'event' ) {echo 'class="current-menu-item"';} ?> ><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
                 <li><a href="#">Campuses</a></li>
                 <li <?php if(get_post_type() == 'post') { echo 'class="current-menu-item"'; } ?> ><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
               </nav>
