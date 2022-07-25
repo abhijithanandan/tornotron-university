492a2da41c9b1ab7bfd5dc11ca48fc5ff738d79f
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2022-07-25 21:15:47 +0530

    Added past Events Page

--- app/public/wp-content/mu-plugins/university-post-type.php
+++ app/public/wp-content/mu-plugins/university-post-type.php
@@ -1,18 +1,19 @@
 <?php
-function university_post_types() {
+function university_post_types()
+{
     register_post_type('event', array(
         'show_in_rest' => true,
-        'supports' => array('title', 'editor','excerpt'),
+        'supports' => array('title', 'editor', 'excerpt'),
         'rewrite' => array('slug' => 'events'),
         'has_archive' => true,
-        'public' => true, 
+        'public' => true,
         'labels' => array(
             'name' => 'Events',
-            'all_item' => 'All Events', 
-            'edit_item' => 'Edit Event', 
-            'singular_name' => 'Event', 
+            'all_item' => 'All Events',
+            'edit_item' => 'Edit Event',
+            'singular_name' => 'Event',
             'add_new_item' => 'Add New Event'
-        ), 
+        ),
         'menu_icon' => 'dashicons-calendar'
     ));
 }
--- app/public/wp-content/themes/tornotron-university-theme/page-past-events.php
+++ app/public/wp-content/themes/tornotron-university-theme/page-past-events.php
@@ -7,20 +7,37 @@ get_header();
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
     <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">
-            <?php
-            the_archive_title();
-            ?> </h1>
+        <h1 class="page-banner__title">Past Events</h1>
         <div class="page-banner__intro">
-            <p><?php the_archive_description(); ?></p>
+            <p>A recap of past events</p>
         </div>
     </div>
 </div>
 
 <div class="container container--narrow page-section">
     <?php
-    while (have_posts()) {
-        the_post();
+
+
+    $today = date('Ymd');
+    $pastEvents = new WP_Query(array(
+        'posts_per_page' => 2,
+        'paged' => get_query_var('paged', 1),
+        'post_type' => 'event',
+        'order' => 'DESC',
+        'meta_key' => 'event_date',
+        'orderby' => 'meta_value_num',
+        'meta_query' => array(
+            array(
+                'key' => 'event_date',
+                'compare' => '<',
+                'value' => $today,
+                'type' => 'numeric'
+            )
+        )
+    ));
+
+    while ($pastEvents->have_posts()) {
+        $pastEvents->the_post();
     ?>
         <div class="event-summary">
             <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
@@ -37,12 +54,11 @@ get_header();
         </div>
     <?php
     }
-    echo paginate_links();
+    echo paginate_links(array(
+        'total' => $pastEvents->max_num_pages
+    ));
     ?>
-    <hr class="section-break">
-    <p>Looking for a recap of the past events? <a href="<?php echo site_url('past-events'); ?>">Checkout our past events here!!!</a></p>
 </div>
-
 <?php
 
 
